ee1631ef6c5866c39271f6f16ff58ac976d5064e
Binashir Rofiah <47813406+rofibins@users.noreply.github.com>
2021-11-15 08:05:08 +0700

    Update swape_case.py
    Update without built in Function

--- swape_case.py
+++ swape_case.py
@@ -1,6 +1,20 @@
-def swape(swape_in):
-    print("Your output word: " + swape_in.swapcase())
+def toggle(word_in):
+    alphabet_helper = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"
+    word_out = ''
+    for x in word_in:
+        for pos in range(52):
+            if alphabet_helper[pos] == x:
+                i = pos
+        if x not in alphabet_helper:
+            word_out += x
+        elif i >=26:
+            word_out += alphabet_helper[i-26]
+        else:
+            word_out += alphabet_helper[i+26]
+    return word_out
+
 
 if __name__ == "__main__":
-    swape_in = input("Please input your word: ")
-    swape(swape_in)
+    word_in = input("Enter your word: ")
+    print("Result : ", end = ' ')
+    print(toggle(word_in))
